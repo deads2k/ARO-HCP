acbc36112945049dd9018a8593cf84c653d078a8
David Eads <deads@redhat.com>
2025-07-08 15:18:38 -0400

    Create aro-hcp-tests using openshift-test-extensions
        https://github.com/openshift-eng/openshift-tests-extension is a library
        designed to create suites of ginkgo tests selected by criteria.  Then it
        will run the in parallel in distinct processes to avoid dependencies.
        It will produce junit output compatible with OCP's CI post-processing.

---
+++ test/e2e/complete_cluster_create.go
@@ -0,0 +1,54 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	api "github.com/Azure/ARO-HCP/internal/api/v20240610preview/generated"
+	"github.com/Azure/ARO-HCP/test/util/labels"
+)
+
+var _ = Describe("Customer", func() {
+	var (
+		clustersClient *api.HcpOpenShiftClustersClient
+	)
+
+	BeforeEach(func() {
+		By("Preparing HCP clusters client")
+		clustersClient = clients.NewHcpOpenShiftClustersClient()
+	})
+
+	It("should be able to create an HCP cluster using bicep templates",
+		labels.Any,
+		labels.Critical,
+		labels.Positive,
+		func(ctx context.Context) {
+			clusterName := "non-existing-cluster"
+			customerRGName := "non-existing-group"
+			var (
+				clusterResource api.HcpOpenShiftCluster
+				clusterOptions  *api.HcpOpenShiftClustersClientBeginCreateOrUpdateOptions
+			)
+			By("Sending put request to create HCPOpenshiftCluster")
+			_, err := clustersClient.BeginCreateOrUpdate(ctx, customerRGName, clusterName, clusterResource, clusterOptions)
+			Expect(err).ToNot(BeNil())
+			errMessage := "RESPONSE 500: 500 Internal Server Error"
+			Expect(err.Error()).To(ContainSubstring(errMessage))
+		})
+})
