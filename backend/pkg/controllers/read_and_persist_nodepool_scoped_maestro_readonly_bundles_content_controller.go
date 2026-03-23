56605953e45f97e8c2d8961bd3033311502571d4
Miguel Soriano <msoriano@redhat.com>
2026-03-23 21:17:23 +0100

    fix: maestro client leaks on read and persist maestro readonly bundles controllers

--- backend/pkg/controllers/read_and_persist_nodepool_scoped_maestro_readonly_bundles_content_controller.go
+++ backend/pkg/controllers/read_and_persist_nodepool_scoped_maestro_readonly_bundles_content_controller.go
@@ -113,14 +113,14 @@ func (c *readAndPersistNodePoolScopedMaestroReadonlyBundlesContentSyncer) SyncOn
 	if err != nil {
 		return utils.TrackError(fmt.Errorf("failed to get Cluster Provision Shard from Cluster Service: %w", err))
 	}
-	maestroClient, err := createMaestroClientFromCSProvisionShard(ctx, c.maestroSourceEnvironmentIdentifier, c.maestroClientBuilder, clusterProvisionShard)
-	if err != nil {
-		return utils.TrackError(fmt.Errorf("failed to create Maestro client: %w", err))
-	}
 	// We create a new context with a cancel function so we can cancel the Maestro client when the sync is done.
 	// This is important to avoid leaking resources when the sync is done.
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+	maestroClient, err := createMaestroClientFromCSProvisionShard(ctx, c.maestroSourceEnvironmentIdentifier, c.maestroClientBuilder, clusterProvisionShard)
+	if err != nil {
+		return utils.TrackError(fmt.Errorf("failed to create Maestro client: %w", err))
+	}
 
 	var syncErrors []error
 	for _, maestroBundleReference := range existingServiceProviderNodePool.Status.MaestroReadonlyBundles {
