11dfe21e2caf0bc6881e29fe222df804edd3b420
Marek Vacula <mvacula@redhat.com>
2026-04-13 17:52:07 +0200

    MustFilter for testing in STG

--- test/cmd/aro-hcp-tests/main.go
+++ test/cmd/aro-hcp-tests/main.go
@@ -200,7 +200,8 @@ func setupCli() *cobra.Command {
 	// 	specs = specs.AddLabel("SLOW")
 
 	// Specs can be globally filtered...
-	// specs = specs.MustFilter([]string{`name.contains("filter")`})
+	// TODO: remove after PR validation
+	specs = specs.MustFilter([]string{`name.contains("Customer should not be able to perform various invalid operations on cluster resources")`})
 
 	// Or walked...
 	// specs = specs.Walk(func(spec *extensiontests.ExtensionTestSpec) {
